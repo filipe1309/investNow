90c038f28999bc11257ee984b5fb072c1dbf850d
filipe1309 <filipe1309@gmail.com>
2018-03-27 19:16:45 -0300

    Add AleartDialog to show detailed information about the indicator

--- app/src/main/java/br/pucpr/appdev/tcc/investnow/adapter/IndicatorsAdapter.java
+++ app/src/main/java/br/pucpr/appdev/tcc/investnow/adapter/IndicatorsAdapter.java
@@ -1,7 +1,10 @@
 package br.pucpr.appdev.tcc.investnow.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +14,14 @@ import android.widget.TextView;
 import java.util.List;
 
 import br.pucpr.appdev.tcc.investnow.R;
+import br.pucpr.appdev.tcc.investnow.model.GovernmentBond;
 import br.pucpr.appdev.tcc.investnow.model.Indicator;
 
 public class IndicatorsAdapter extends RecyclerView.Adapter<IndicatorsAdapter.IndicatorViewHolder>{
 
-    private List<Indicator> indicators;
+    public List<Indicator> indicators;
     private int rowLayout;
-    private Context context;
+    public Context context;
     //public static final String IMAGE_URL_BASE_PATH="http://image.tmdb.org/t/p/w342//";
 
     public IndicatorsAdapter(List<Indicator> indicators, int rowLayout, Context context) {
@@ -28,7 +32,7 @@ public class IndicatorsAdapter extends RecyclerView.Adapter<IndicatorsAdapter.In
 
     //A view holder inner class where we get reference to the views in the layout using their ID
 
-    public static class IndicatorViewHolder extends RecyclerView.ViewHolder {
+    public class IndicatorViewHolder extends RecyclerView.ViewHolder {
         LinearLayout indicatorLayout;
         TextView indicatorTitle;
         TextView dataUpdate;
@@ -40,6 +44,52 @@ public class IndicatorsAdapter extends RecyclerView.Adapter<IndicatorsAdapter.In
             indicatorTitle = (TextView) v.findViewById(R.id.title);
             dataUpdate = (TextView) v.findViewById(R.id.date_update);
             rateValue = (TextView) v.findViewById(R.id.rate_value);
+
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // get position
+                    int pos = getAdapterPosition();
+
+                    // check if item still exists
+                    if(pos != RecyclerView.NO_POSITION){
+                        Log.d("Click", "pos: " + pos);
+                        Indicator clickedDataItem = indicators.get(pos);
+                        Log.d("Click", "You clicked " + clickedDataItem.getTitle());
+                        //Toast.makeText(v.getContext(), "You clicked " + clickedDataItem.getName(), Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(view.getContext(), "You clicked " + clickedDataItem.getTitle(), Toast.LENGTH_SHORT).show();
+
+                        // 1. Instantiate an AlertDialog.Builder with its constructor
+                        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+
+                        //builder.setCancelable(true);
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                return;
+                            }
+                        });
+
+                        // 2. Chain together various setter methods to set the dialog characteristics
+                        String message = "Rendimento: " + clickedDataItem.getRateValue() + "\n";
+                        if (clickedDataItem.getDailyValue() != null) {
+                            message += "Valor diário: " + clickedDataItem.getDailyValue() + "\n";
+                        }
+                        if (clickedDataItem.getDailyDateFormatted() != null) {
+                            message += "Data: " + clickedDataItem.getDailyDateFormatted() + "\n";
+                        }
+                        if (clickedDataItem.getRateDateFormatted() != null) {
+                            message += "Data atualização do indíce: " + clickedDataItem.getRateDateFormatted() + "\n";
+                        }
+                        builder.setMessage(message)
+                                .setTitle(clickedDataItem.getTitle());
+
+                        // 3. Get the AlertDialog from create()
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+                }
+            });
         }
     }
 
